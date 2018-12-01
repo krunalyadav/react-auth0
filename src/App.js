79bb058621cff0a333ca128838ad6b0dd8c9e264
Krunal Yadav <krunal.yadav18@gmail.com>
2018-12-01 19:59:35 +0530

    Add navigation and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <>
+        <Nav />
+        <div className="body">
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
+    );
   }
 }
 
