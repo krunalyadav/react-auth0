2b33a308b4b05d2953c22bcc5a9525664261c5ec
Krunal Yadav <krunal.yadav18@gmail.com>
2018-12-05 11:17:41 +0530

    Add login functionality to application

---
+++ src/Callback.js
@@ -0,0 +1,21 @@
+import React, {
+    Component
+} from 'react';
+
+class Callback extends Component {
+
+    componentDidMount() {
+        if(/access_token|id_token|error/.test(this.props.location.hash)) {
+            this.props.auth.handleAuthentication();
+        }
+        else{
+            throw new Error('invalid callback url')
+        }
+    }
+
+    render() {
+        return <h1>Loading..</h1>;
+    }
+}
+
+export default Callback;
