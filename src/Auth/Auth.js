116e74f2eea476581b6554408533d8b248287068
Krunal Yadav <krunal.yadav18@gmail.com>
2018-12-06 11:01:48 +0530

    Add user profile to the application

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,6 +3,7 @@ import auth0 from 'auth0-js';
 export default class Auth {
   constructor(history) {
     this.history = history;
+    this.userProfile = null;
     this.auth0 = new auth0.WebAuth({domain: process.env.REACT_APP_AUTH0_DOMAIN, clientID: process.env.REACT_APP_AUTH0_CLIENTID, redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL, responseType: 'token id_token', scope: 'openid profile email'});
   }
 
@@ -38,5 +39,32 @@ export default class Auth {
   isAuthenticated() {
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  logout = () => {
+    localStorage.removeItem("access_token")
+    localStorage.removeItem("id_token")
+    localStorage.removeItem("expires_at")
+    this.userProfile = null;
+    this.auth0.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENTID,
+      returnTo:'http://localhost:3000'
+    })
+  }
+
+  getAccessToken = () => {
+    const accessToken = localStorage.getItem('access_token');
+    if(!accessToken){
+      throw new Error('No access toekn found.')
+    }
+    return accessToken;
+  }
+
+  getProfile = cb =>{
+    if(this.userProfile) return cb(this.userProfile);
+    this.auth0.client.userInfo(this.getAccessToken(),(err,profile)=>{
+      if(profile) this.userProfile = profile;
+      cb(profile,err);
+    })
   }
 }
